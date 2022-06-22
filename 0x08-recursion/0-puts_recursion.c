37269836d4485fef43ba1e2db897fa7c12e972e8
Anastacia Warigi <anastaciawarigi@gmail.com>
2022-06-22 05:15:49 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "Main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+		if (*s == '\0')
+		{
+			_putchar('\n');
+			return;
+		}
+		_putcha(*s);
+		_puts_recursion(s + 1);
+}	
